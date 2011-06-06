102d2a804c85659b03065d81413d81f7fa8d594a
Lari Kirby <larikirby@fastmail.fm>
2011-06-06 12:44:01 -0400

    new query form displays now. ugly & not very functional, but it displays.

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -9,19 +9,20 @@ class QueriesController < ApplicationController
     # include results??
     # no - ask the user; need to ask browse(html) or download(csv) too
 
-    # # so results code actualy belongs to allcombinedcontroller#index or how about resultslist#show ?
+    # # so results code actually belongs to allcombinedcontroller#index or how about resultslist#show ?
     # @search = VwcAllCombined.search(params[:search])  
-    # @results = @search.relation.projects(params[:selected_fields])  
+    # @results = @search.relation.select("distinct #{@query.selected_fields.join(', ')}")  
   end
 
   def new
     # display form for creating a new query
     @title = "New Query"
-    @query = Query.new
-    @query.selected_fields = [:region_code]
+    # @query = Query.new
+    # @query.selected_fields = [:region_code]
+    # @search = VwcAllCombined.search()
+    @field_info = VwcAllCombined.field_info
     # @query.vwc_all_combined = MetaSearch::Searches::VwcAllCombined.new(VwcAllCombined)
-    # VwcAllCombined.search()
-    # @results = @filters.select("distinct #{@query.selected_fields.join(', ')}").limit(10)
+    # @results = @search.select("distinct #{@query.selected_fields.join(', ')}").limit(10)
   end
 
   def create
